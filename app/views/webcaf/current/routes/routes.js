9f0425d0ee819825be4bea4a9d7602babe7aa691
Robert Nagy <robertc.nagy@nagyseo.com>
2025-07-22 22:02:19 +0100

    Adding org-details journey and fixing data updates and added logic on B4b

--- app/views/webcaf/current/routes/routes.js
+++ app/views/webcaf/current/routes/routes.js
@@ -80,6 +80,47 @@ router.post('/b3a-answer', function(request, response) {
     }
 })
 
+// Q: b3a - routing
+router.post('/b4b-answer', function(request, response) {
+
+    var achieved1 = request.session.data['b4b-outcome1-achieved']
+    var achieved2 = request.session.data['b4b-outcome2-achieved']
+    var achieved3 = request.session.data['b4b-outcome3-achieved']
+    var achieved4 = request.session.data['b4b-outcome4-achieved']
+    var achieved5 = request.session.data['b4b-outcome5-achieved']
+    var achieved6 = request.session.data['b4b-outcome6-achieved']
+    var achieved7 = request.session.data['b4b-outcome7-achieved']
+    var achieved8 = request.session.data['b4b-outcome8-achieved']
+    var partiallyachieved1 = request.session.data['b4b-outcome1-partiallyachieved']
+    var partiallyachieved2 = request.session.data['b4b-outcome2-partiallyachieved']
+    var partiallyachieved3 = request.session.data['b4b-outcome3-partiallyachieved']
+    var partiallyachieved4 = request.session.data['b4b-outcome4-partiallyachieved']
+    var partiallyachieved5 = request.session.data['b4b-outcome5-partiallyachieved']
+    var partiallyachieved6 = request.session.data['b4b-outcome6-partiallyachieved']
+    var notachieved1 = request.session.data['b4b-outcome1-not-achieved']
+    var notachieved2 = request.session.data['outcome2-not-achieved']
+    var notachieved3 = request.session.data['b4b-outcome3-not-achieved']
+    var notachieved4 = request.session.data['b4b-outcome4-not-achieved']
+    var notachieved5 = request.session.data['b4b-outcome5-not-achieved']
+    
+    
+    
+    
+    if (achieved1 == "yes" &&  achieved2 == "yes" &&  achieved3 == "yes" &&  achieved4 == "yes" &&  achieved5 == "yes" &&  achieved6 == "yes" &&  achieved7 == "yes"&&  achieved8 == "yes" &&  notachieved1 == "no" &&  notachieved2 == "no" &&  notachieved3 == "no" &&  notachieved4 == "no" &&  notachieved5 == "no" ) {
+        response.redirect("b4b-outcome-achieved")
+    }
+    else if (partiallyachieved1 == "yes" &&  partiallyachieved2 == "yes" &&  partiallyachieved3 == "yes" &&  partiallyachieved4 == "yes" &&  partiallyachieved5 == "yes" &&  partiallyachieved6 == "yes" &&  notachieved1 == "no" &&  notachieved2 == "no" &&  notachieved3 == "no" &&  notachieved4 == "no" &&  notachieved5 == "no")
+     {
+        response.redirect("b4b-outcome-partially-achieved")
+    }
+    else 
+     {
+        response.redirect("b4b-outcome-not-achieved")
+    }
+})
+
+
+
 module.exports = router
 
  
